c9a143ddcbd17b939219909b0dfecf808e1fa1ce
Goga-rid <wopa22@list.ru>
2023-06-18 06:49:21 +0300

    connecting the api key and fixing minor details

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-//var queryURL = `"https://api.currencyapi.com/v3/latest?apikey=" +  ${APIKey} + "&currencies=" + ${country}}`;
-//https://api.currencyapi.com/v3/latest?apikey=A029StvJl0JE8mEktoDdadbzgg70ipzxccdHrJPb&currencies=EUR
+// const valueInputNode = document.querySelector('.input-group mb-3');
+let currencies = document.querySelector('#currencies');
+console.log(currencies);
+
+let baseCurr = document.querySelector('#base_currency');
+const APIKey = 'A029StvJl0JE8mEktoDdadbzgg70ipzxccdHrJPb';
+
+
+let queryURL = `"https://api.currencyapi.com/v3/latest?apikey=${APIKey}&currencies=${currencies}&base_currency=${baseCurr}"`;
+
+console.log(queryURL)
+    currencies.addEventListener('input', () => {
+        console.log(queryURL)
+    });
