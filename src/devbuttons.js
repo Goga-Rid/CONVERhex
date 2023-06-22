3eb66e208312f793d0fbfa4e2324ed1e77365bdf
MaloVer1471 <ig.lager@yandex.ru>
2023-06-22 17:08:00 +0300

    revers button update(with linter)

--- src/devbuttons.js
+++ src/devbuttons.js
@@ -1,10 +1,10 @@
 function swapSelectedOptions() {
-    var leftSelect = document.getElementById("leftSelect");
-    var rightSelect = document.getElementById("rightSelect");
+  const leftSelect = document.getElementById('leftSelect');
+  const rightSelect = document.getElementById('rightSelect');
 
-    var value1 = leftSelect.value;
-    var value2 = rightSelect.value;
+  const value1 = leftSelect.value;
+  const value2 = rightSelect.value;
 
-    leftSelect.value = value2;
-    rightSelect.value = value1;
-  }
+  leftSelect.value = value2;
+  rightSelect.value = value1;
+}
