8e806181d0f5eb99dec5cf43e746eec0f8387425
MaloVer1471 <ig.lager@yandex.ru>
2023-06-22 17:05:59 +0300

    revers button update

--- src/converter.js
+++ src/converter.js
@@ -1,7 +1,1 @@
-<<<<<<< HEAD
-const RATES = {
-
-};
-=======
 const RATES = { };
->>>>>>> 437b01d31692c9769d27edc867943c8bdd3605cd
--- src/devbuttons.js
+++ src/devbuttons.js
@@ -1,33 +1,10 @@
-const selectLeft = document.getElementById('leftSelect'); // <- обращение к левому select
-const selectRight = document.getElementById('rightSelect'); // <- обращение к правому select
+function swapSelectedOptions() {
+    var leftSelect = document.getElementById("leftSelect");
+    var rightSelect = document.getElementById("rightSelect");
 
-const valueSelectLeft = selectLeft.value; // <- берём value выбранной валюты в левом select
-const valueSelectRight = selectRight.value; // <- берём value выбранной валюты в правом select
+    var value1 = leftSelect.value;
+    var value2 = rightSelect.value;
 
-const emptyValueLeft = valueSelectLeft; // <- пустышка, принимающая значение value левой валюты
-const emptyValueRight = valueSelectRight; // <- пустышка, принимающая значение value правой валюты
-
-const clickEvent = document.querySelector('#reversButton'); // <- Обращение к кнопке
-const clickCount = 0; // <- счётчик для click ивента
-
-clickEvent.on('click', (clickCount) => {
-  clickCount += 1;
-  return clickCount;
-}); // <- Реализация click ивента
-
-const reversValue = (clickCount, valueSelectLeft, valueSelectRight, emptyValueLeft, emptyValueRight) => {
-  if (clickCount % 2 === 0) {
-    valueSelectLeft = emptyValueRight;
-    valueSelectRight = emptyValueLeft;
-    return (valueSelectLeft, valueSelectRight);
+    leftSelect.value = value2;
+    rightSelect.value = value1;
   }
-  valueSelectLeft = emptyValueLeft;
-  valueSelectRight = emptyValueRight;
-  return (valueSelectLeft, valueSelectRight);
-};
-
-const reversButton = (valueSelectLeft, valueSelectRight, selectLeft, selectRight) => {
-  selectLeft.value = valueSelectLeft;
-  selectRight.value = valueSelectRight;
-  return (selectLeft, selectRight);
-};
